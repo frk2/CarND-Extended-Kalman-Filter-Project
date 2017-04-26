659d17021bd57944ed49792a057c8ee760b08e80
Faraz Khan <farazrkhan@gmail.com>
2017-04-25 22:03:19 -0700

    - Working EKF

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,4 +1,5 @@
 #include "kalman_filter.h"
+#include <math.h>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -9,31 +10,65 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+    x_ = x_in;
+    P_ = P_in;
+    F_ = F_in;
+    H_ = H_in;
+    R_ = R_in;
+    Q_ = Q_in;
+}
+
+void KalmanFilter::Update(const VectorXd &z, bool extended) {
+    /**
+    TODO:
+      * update the state by using Kalman Filter equations
+    */
+    VectorXd z_pred = VectorXd(3);
+    if (extended) {
+        double rs;
+        rs = sqrt(pow(x_[0], 2) + pow(x_[1], 2));
+        z_pred << rs,
+                atan2(x_[1], x_[0]),
+                (x_[0] * x_[2] + x_[1] * x_[3]) / rs;
+
+    } else {
+        z_pred = H_ * x_;
+    }
+    VectorXd y = z - z_pred;
+    if (extended) {
+        while (y[1] > M_PI) {
+            y[1] -= 2 * M_PI;
+        }
+        while (y[1] < -M_PI) {
+            y[1] += 2 * M_PI;
+        }
+    }
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * Si;
+
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
-}
-
-void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+    /**
+    TODO:
+      * predict the state
+    */
+    x_ = F_ * x_;
+    MatrixXd Ft = F_.transpose();
+    P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+    /**
+    TODO:
+      * update the state by using Extended Kalman Filter equations
+    */
 }
